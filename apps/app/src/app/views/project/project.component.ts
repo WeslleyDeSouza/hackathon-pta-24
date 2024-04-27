ddc6d243ce33352cfd7fb548b42707f540a2e3c5
Wes <we.desouza@hotmail.com>
2024-04-27 23:23:44 +0200

    Current User current Tenant

--- apps/app/src/app/views/project/project.component.ts
+++ apps/app/src/app/views/project/project.component.ts
@@ -5,7 +5,7 @@ import { ProjectApi } from './common/project.api';
   standalone: true,
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './project.component.scss',
   imports: [],
   providers:[ProjectApi]
 })
