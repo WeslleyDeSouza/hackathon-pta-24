a39d57e900bbf07c1e72b39daa770f42dc19a70f
Wes <we.desouza@hotmail.com>
2024-04-28 02:25:24 +0200

    UserStory

--- apps/app/src/app/views/user/user-story/user-story.component.ts
+++ apps/app/src/app/views/user/user-story/user-story.component.ts
@@ -1,10 +1,11 @@
-import { Component, TemplateRef, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { PageBase } from '../../view.base';
 import { ActivatedRoute } from '@angular/router';
 import { NgForOf, NgIf } from '@angular/common';
 import { UserStoryDtoResponse, UserStoryService } from '@hackathon-pta/app/api';
 
 const isStateOpenForReview = (item:UserStoryDtoResponse) => item.stateOpenForReview;
+const isStateReviewed = (item:UserStoryDtoResponse) => false
 
 @Component({
   standalone: true,
@@ -14,31 +15,48 @@ const isStateOpenForReview = (item:UserStoryDtoResponse) => item.stateOpenForRev
   imports: [
     NgForOf,
     NgIf
-  ]
+  ],
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
 export class UserStoryComponent extends PageBase{
 
   route = inject(ActivatedRoute)
+
+  states = {
+    hasOpenStoriesForReview:false,
+    hasOpenStoriesReviewed:false
+  }
 
 
   constructor(protected api:UserStoryService) {
     super();
   }
 
+  get hasStateOpenStoriesForReview():boolean{
+    return  this.states.hasOpenStoriesForReview
+  }
+
   get stories():Array<UserStoryDtoResponse>{
       return this.route.snapshot.data['stories']
   }
   get storiesOpenForReview():Array<UserStoryDtoResponse>{
-      return this.stories.filter( isStateOpenForReview )
+    let stories = this.stories.filter( isStateOpenForReview );
+    this.states.hasOpenStoriesForReview = !!stories.length
+    return stories
   }
 
-
-  override getData():void {
-    console.log(this.stories[0]);
+  get storiesReviewed():Array<UserStoryDtoResponse>{
+    let stories = this.stories.filter( isStateReviewed );
+    this.states.hasOpenStoriesReviewed = !!stories.length
+    return stories
   }
 
   get hasAdminRight():boolean {
     return  true
+  }
+
+  override getData():void {
+    console.log(this.stories[0]);
   }
 
   onSetStateOpenForReview(userStory:UserStoryDtoResponse):void{
